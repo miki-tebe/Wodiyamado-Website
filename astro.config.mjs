47c2fb80ae0eb3a4dde98cfdac931b317935f6df
Mikiyas Tibebu <mikitebe123@gmail.com>
2024-01-28 19:54:25 +0300

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,13 @@ import tailwind from "@astrojs/tailwind";
 import keystatic from "@keystatic/astro";
 import markdoc from "@astrojs/markdoc";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    markdoc(),
-    keystatic(),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), markdoc(), keystatic()],
   output: "hybrid",
+  adapter: vercel()
 });
