412cd4397c6823b76de9eba7e6886da5a1a0aa5a
Mikiyas Tibebu <mikitebe123@gmail.com>
2024-02-06 12:45:17 +0300

    Update storage kind to cloud and add cloud project configuration

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -3,7 +3,10 @@ import { config, fields, collection } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
+    kind: "cloud",
+  },
+  cloud: {
+    project: "wodiyamado/wodiyamado-website",
   },
   collections: {
     events: collection({
