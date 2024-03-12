a7e01b78063d10302f1c4a8ca78577e055fdab0e
Mikiyas Tibebu <mikitebe123@gmail.com>
2024-03-12 15:15:30 +0300

    Update package dependencies, add Accordion component and update about page

---
+++ src/components/FAQComponent.tsx
@@ -0,0 +1,248 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+export default function FAQComponent() {
+  return (
+    <Accordion
+      type="single"
+      collapsible
+      className="w-full"
+      defaultValue="item-1"
+    >
+      <AccordionItem value="item-1">
+        <AccordionTrigger>What is rotary?</AccordionTrigger>
+        <AccordionContent>
+          Rotary started with the vision of one man paul harris on February 23,
+          1905 so professionals with diverse backgrounds could exchange ideas
+          and form meaningful, lifelong friendship. Overtime, rotary's reach and
+          vision gradually extended to humanitarian service. Members have a long
+          track record if addressing challenges in their community and around
+          the world. Only 16 years after being founded, rotary had clubs on 6
+          continents and members now span the globe working to solve some of
+          world's most challenging problems. Rotary is made up of three parts
+          clubs, rotary international and the rotary foundation.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>What does rotary do?</AccordionTrigger>
+        <AccordionContent>
+          Rotary is an international organization creating opportunities for
+          professionals and those in business to use their vocational skills in
+          assisting their communities the world over. Rotarians contribute time
+          and expertise to make a difference – helping those who need it and
+          encouraging youth to reach their potential. Members believe that they
+          have a shared responsibility to take action on world's most persistent
+          issues. Clubs work together to: promote peace, fight disease, provide
+          clean water, sanitation and hygiene, save mother's and children,
+          support education, grow local economies, and protect the environment.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>What is rotaract?</AccordionTrigger>
+        <AccordionContent>
+          Rotaract originally began as a rotary international youth program i
+          1968 at Charlotte north rotary club in Charlotte and has grown into a
+          major organization of 11,772 clubs spread around the world and nearly
+          170,000 members in 189 countries and geographic areas. It is a service
+          leadership, professional and community service organization for young
+          adults aged 18 and over. Rotaract" stands for "Rotary in Action",
+          although the name originally comes from a combination of "Rotary" and
+          "Interact" (International + Action), the high school level program
+          created by Rotary International in 1962. Most Rotaract activities take
+          place at the club level. Rotaract clubs hold formal meetings in person
+          or virtually, usually every two weeks, which feature speakers, special
+          outings, social activities, discussions or visits to other clubs. Club
+          members get together on designated days for service project work,
+          social events, or professional/leadership development workshops. To be
+          eligible for membership, prospective members must be 18 years of age
+          and over, show that they are committed to Rotaract, and show that they
+          are of good standing in the community. After being approved by the
+          club, prospective members are 'inducted' to become members, also known
+          as 'Rotaractors'. The avenues of service include Club Service,
+          Community Service, International Service and Professional Development.
+          Rotaract Club undertake various projects for the overall development
+          of its members and the community. Rotaract club may, but is not
+          required to, establish upper age limits, provided that the club
+          obtains the concurrence of its members records it in their bylaws.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-4">
+        <AccordionTrigger>What is interact?</AccordionTrigger>
+        <AccordionContent>
+          Interact clubs bring together young people ages 12-18 to develop
+          leadership skills while discovering the power of Service Above Self.
+          Interact clubs organize at least two projects every year, one that
+          helps their school or community and one that promotes international
+          understanding. Rotary club sponsors mentor and guide Interactors as
+          they carry out projects and develop leadership skills. Interact clubs
+          are sponsored by individual Rotary clubs, which provide support and
+          guidance, but they are self-governing and self-supporting. Club
+          membership varies greatly. Clubs can be single gender or mixed, large
+          or small.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-5">
+        <AccordionTrigger>
+          What are clubs and how many are there?
+        </AccordionTrigger>
+        <AccordionContent>
+          There are more than 8,200 Rotaract clubs in over 150 countries
+          worldwide! Rotaract is an integral part of Rotary International.
+          Rotaract clubs bring together people ages 18 and older to exchange
+          ideas with leaders in the community, develop leadership and
+          professional skills, and have fun through service. In communities
+          worldwide, Rotary and Rotaract members work side by side to take
+          action through service. From big cities to rural villages, Rotaract is
+          changing communities like yours. Rotaract members decide how to
+          organize and run their clubs, manage their own funds, and plan and
+          carry out activities and service projects. Rotary club sponsors offer
+          guidance and support and work with a club as partners in service.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-6">
+        <AccordionTrigger>
+          How many rotaract clubs are there in Ethiopia?
+        </AccordionTrigger>
+        <AccordionContent>
+          Rotary Ethiopia reached 22 Rotary clubs, 16 Rotaract and 14 interact
+          clubs across the country with about 2000 members
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-7">
+        <AccordionTrigger>How to join?</AccordionTrigger>
+        <AccordionContent>
+          Clubs accept members by invitation. To help find the right club for
+          you you'll be asked a few questions about yourself and your interest.
+          Then they will share your answers with rotaract leaders in your
+          community who will match you with a club.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-8">
+        <AccordionTrigger>
+          What is the Rotaract Club of Wodiyamado?
+        </AccordionTrigger>
+        <AccordionContent>
+          The Rotaract Club of Wodiyamado, established by a group of 17
+          enthusiastic young professionals on November 13, 2009, Currently, the
+          club is primarily dedicated to a collaborative project with
+          Fiker-Ethiopia National Association of Intellectual Disability
+          (FENAID). FENAID focuses on training individuals, specifically
+          children and young adults with Down syndrome, while also serving as an
+          awareness center for the community. As part of the FENAID family, the
+          Rotaract Club of Wodiyamado has been actively involved in fundraising
+          efforts for the association by hosting diverse events.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-9">
+        <AccordionTrigger>
+          What are the rotary and rotaract projects?
+        </AccordionTrigger>
+        <AccordionContent>
+          Rotaract focuses on the development of young adults as leaders in
+          their communities and workplaces. Clubs around the world also take
+          part in international service projects, in a global effort to bring
+          peace and international understanding to the world. We use our
+          knowledge of local issues to identify areas of need, then apply our
+          expertise and diverse perspectives to find a solution. Rotary members
+          likely are working in your community right now to feed the hungry,
+          tutor disadvantaged children, maintain parks and playgrounds, and much
+          more. Rotaractors in Africa are taking action to create lasting change
+          in themselves and communities like peace building and conflict
+          prevention, water sanitation and hygiene .
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-10">
+        <AccordionTrigger>What is induction?</AccordionTrigger>
+        <AccordionContent>
+          The purpose of the Induction Ceremony is to let you know how important
+          it is to have you as a new member, and to remind you that being a
+          Rotarian involves both inherent privileges and certain obligations.The
+          induction of new members is a special opportunity to welcome people
+          into your club. This step is a memorable experience that helps the new
+          members feel like they're a part of something important—your club.
+          When you invest in their introduction, you let new members know
+          they're valued and appreciated.The principles, responsibilities and
+          obligations of Rotary have been explained to you, and you have
+          expressed your willingness to become a Rotarian. No one is eligible to
+          become a Rotarian unless, in his business or professional life, he/she
+          endeavors to practice the principles of goodwill and service.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-11">
+        <AccordionTrigger>What is the hierarchy in rotaract?</AccordionTrigger>
+        <AccordionContent>
+          Rotaract club's board of directors consists of the following elected
+          officers: president, immediate past president, vice president,
+          secretary, treasurer, and additional officers deemed necessary by the
+          president and the club. The board is the governing body for the club
+          and meets at least once a month.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-12">
+        <AccordionTrigger>What is board of director?</AccordionTrigger>
+        <AccordionContent>
+          The board of directors serves as the governing body of a Rotaract club
+          and includes the president, immediate past president, vice president,
+          secretary, treasurer, and any additional officers your club decides
+          are needed. Officers' duties and additional roles should be defined in
+          your club bylaws. The club's board includes between 3 and 8 directors
+          that are chairpersons of committees dedicated to carrying out the
+          activities and responsibilities of our club. Often, these areas are
+          separated into Club Administration, Rotary Foundation, Public Image,
+          Membership, and Service Projects.The primary responsibility of all
+          directors is to contribute toward the success of the vision, goals and
+          activities of the Club. They are expected to delegate, coordinate, and
+          monitor various Club operational functions and assure that they are
+          properly accomplished.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-13">
+        <AccordionTrigger>
+          What is the benefit of joining a rotaract club?
+        </AccordionTrigger>
+        <AccordionContent>
+          Leadership Development: Rotaract is an organization of leaders and
+          successful people, serving in Rotaract Positions like a college
+          education - Leadership learning, how to motivate, influence and lead
+          leaders. Continuing Education: Rotaract is a learning opportunity and
+          is a continuous training in doing research and assessment of critical
+          community needs, strategizing, developing plans, creating and managing
+          teams and other resources, raising funds, and celebrating
+          achievements. This is complete and comprehensive education in
+          developing multifarious management skills encompassing management of
+          resources, production, humans, finances, publicity, promotions, and
+          projects. Professional Network: This is a significant advantage which
+          Rotaractors enjoy over other professionals. Everyone needs to network
+          especially when a young boy and girl wish to work as an intern or is
+          looking for a job after studies. Rotary’s network of business people
+          and professionals help them get the desired internship or placement.
+          Career Advancement/Professional Growth: Being a member of the Rotaract
+          Club helps young boys and girls receive instant recognition for their
+          community service projects, which gives them credit rating during
+          further studies abroad or during internship/placements, since Rotaract
+          and its sponsoring organization Rotary enjoys international
+          recognition, and Rotaractors get a headstart. The Opportunity to
+          Serve: Rotaract is service; its business is mankind while its product
+          is service. Rotaractors provide community service to both local and
+          international communities. This is perhaps the best reason for
+          becoming a Rotaractor, the chance to do something for somebody else;
+          to sense the self-fulfillment that comes in the process, and the
+          return of that satisfaction to one's own life. It’s richly rewarded.
+          Ethical Living: Rotaract movement infuses amongst its members the
+          spirit of giving, and ethical behavior, which augurs well generally
+          for the world at large. Participation in National and International
+          Conferences: Rotaractors are eligible to register for any of the
+          Rotary or Rotaract conferences held around the world. Youth Exchange
+          Programme: Rotaractors can also join in many of the youth exchange
+          programmes that Rotary organizes, giving youngsters the opportunity to
+          travel to other countries, attend short term workshops, or long-term
+          academic studies
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
