fef7a2cb24e086b6944c24903320bf5070ec3e33
Mikiyas Tibebu <mikitebe123@gmail.com>
2025-02-24 14:09:03 +0300

    Change storage kind from local to cloud in keystatic configuration

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -3,7 +3,7 @@ import { config, fields, collection } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
+    kind: "cloud",
   },
   cloud: {
     project: "wodiyamado/wodiyamado-website",
