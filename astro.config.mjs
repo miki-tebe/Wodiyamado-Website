2c9409c1ad77e90eb0a41dd343036eaaf5faef97
Mikiyas Tibebu <mikitebe123@gmail.com>
2026-01-16 18:41:54 +0300

    Replace bun lockfile with npm lockfile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 import keystatic from "@keystatic/astro";
 import markdoc from "@astrojs/markdoc";
 import vercel from "@astrojs/vercel";
@@ -11,9 +10,6 @@ import db from "@astrojs/db";
 export default defineConfig({
   integrations: [
     react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
     markdoc(),
     keystatic(),
     db(),
